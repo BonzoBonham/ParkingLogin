40199b9bd112f9c0825d265685940aa2b6b0934b
Sabeto051 <bedoya.santi@gmail.com>
2019-09-23 07:30:13 -0500

    routes 1

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.set('port', process.env.PORT || 3000)
 
 app.use(express.json())
 
+app.use('/api/users', require('./routes/api'))
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Starting the server
