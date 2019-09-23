647bc5d09e400f626f0d7ee977fc1ef3e359f2b1
Sabeto051 <bedoya.santi@gmail.com>
2019-09-23 06:41:57 -0500

    Index js server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const crypto = require("crypto-js");
-const auth = require("google-auth-library");
-const express = require("express");
-const mongoose = require("mongoose");
+const crypto = require('crypto-js')
+const auth = require('google-auth-library')
+const express = require('express')
+const mongoose = require('mongoose')
 
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Starting the server
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`)
+})
