98a2bdd04e692d9eae629644b6479f099a66e577
Sabeto051 <bedoya.santi@gmail.com>
2019-09-23 07:10:35 -0500

    Hash good

--- src/libs/hash.js
+++ src/libs/hash.js
@@ -4,12 +4,12 @@ const hash = crypto.createHash('sha256')
 // hash.update('holi')
 // console.log(hash.digest('hex'))
 
-module.exports = {
-  createPasswordHash: function(plainPassword) {
-    var hash = crypto
-      .createHash('sha256')
-      .update(pwd)
-      .digest('base64')
-    console.log(hash)
-  }
+// module.exports = {
+//   createPasswordHash:
+function createPasswordHash(plainPassword) {
+  var hashed = hash.update(plainPassword).digest('hex')
+  console.log(hashed)
 }
+// }
+
+createPasswordHash('admin')
