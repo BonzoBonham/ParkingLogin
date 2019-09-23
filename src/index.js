adb5b2bcc10c77b1055a7dffd3148ca96f49266f
Sabeto051 <bedoya.santi@gmail.com>
2019-09-23 06:45:32 -0500

    Update script npm start

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const crypto = require('crypto-js')
 const auth = require('google-auth-library')
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
