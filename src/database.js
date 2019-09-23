50c221219fb419d4728bd8d13557c039fa74b950
Sabeto051 <bedoya.santi@gmail.com>
2019-09-23 06:36:45 -0500

    User model

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const URI = 'mongodb://localhost/mern-task'
+const URI = 'mongodb://localhost/parking-db'
 mongoose
   .connect(URI, { useNewUrlParser: true, useFindAndModify: false })
   .then(db => console.log('DB is connected'))
