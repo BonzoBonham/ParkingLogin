e34dc0a3b8673789be6a382205838d5bab2d1c67
Nicolas <pereznicolas90@gmail.com>
2019-09-23 06:44:14 -0500

    holi

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const crypto = require('crypto-js')
-const auth = require('google-auth-library')
-const express = require('express')
-const mongoose = require('mongoose')
+const crypto = require('crypto-js');
+const auth = require('google-auth-library');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting the server
 app.listen(app.get('port'), () => {
-  console.log(`server on port ${app.get('port')}`)
+    console.log(`server on port ${app.get('port')}`);
 })
