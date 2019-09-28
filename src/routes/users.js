cdb7111482935f0302aeff336b6668fd3db8121f
Sabeto051 <bedoya.santi@gmail.com>
2019-09-28 01:04:59 -0500

    adelanto

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,12 +3,17 @@ const router = express.Router()
 const User = require('../models/user')
 const path = require('path')
 const speakeasy = require('speakeasy')
+const QRCode = require('qrcode')
 
 router.post('/create', async (req, res) => {
-  try {
-    let username = req.body.username
-    let password = req.body.password
-
+  let username = req.body.username
+  let password = req.body.password
+  if (
+    username !== '' &&
+    password !== '' &&
+    username !== null &&
+    password !== null
+  ) {
     let secret = speakeasy.generateSecret({ length: 20 })
 
     const user = new User({
@@ -17,16 +22,70 @@ router.post('/create', async (req, res) => {
       secret32: secret.base32,
       uri: secret.otpauth_url
     })
-    res.send({ status: true })
-  } catch (error) {
-    console.log('Error-> ' + error)
-    res.send({ status: false })
+    await user.save()
+    res.send({ status: 'User created' })
+  } else {
+    res.send({ status: 'User not created, ERROR' })
   }
 })
 
-router.get('/debug', async (req, res) => {
-  const users = await User.find()
-  res.send(users)
+router.post('/login', async (req, res) => {
+  let username = req.body.username
+  let password = req.body.password
+
+  if (
+    username !== '' &&
+    password !== '' &&
+    username !== null &&
+    password !== null
+  ) {
+    let dbUser = await User.findOne({ username, password })
+
+    if (dbUser) {
+      let uri = 'pp'
+
+      const generateQR = async text => {
+        try {
+          uri = await QRCode.toDataURL(text)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+
+      await generateQR(dbUser.uri)
+
+      res.json({ status: true, uri })
+    } else {
+      res.json({ status: false })
+    }
+  } else {
+    console.log('--Error--')
+    res.json({ status: false })
+  }
+})
+
+router.post('/token', async (req, res) => {
+  let username = req.body.username
+  let password = req.body.password
+
+  let dbUser = await User.findOne({ username, password })
+
+  if (dbUser) {
+    let validation = speakeasy.totp.verify({
+      secret: dbUser.secret32,
+      encoding: 'base32',
+      token: req.body.token,
+      window: 0
+    })
+    res.json({ validation })
+    // if (validation) {
+    //   res.sendFile('qr.html', {
+    //     root: path.join(__dirname, '../public')
+    //   })
+    // }
+  } else {
+    res.json({ validation: false })
+  }
 })
 
 router.get('/', async (req, res) => {
